f337ead6305a87d50efaec626325491f988c4eb9
GitHub Action <action@github.com>
2020-02-26 19:35:05 +0000

    Add changes

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,2 +1,19 @@
-/// <reference types="react" />
-export declare const App: () => JSX.Element;
+/**
+ * [[include:../manual/tutorial.md]]
+ */
+import React from "react";
+interface Props {
+    bar: string;
+}
+interface State {
+}
+/**
+ * Simple Function Component Test
+ * @param props.bar - test prop
+ */
+export declare const FuncComponent: (props: Props) => JSX.Element;
+export declare class ClassComponent extends React.Component<Props, State> {
+    constructor(props: Props);
+    render(): JSX.Element;
+}
+export {};
--- build/index.js
+++ build/index.js
@@ -1,5 +1,23 @@
+/**
+ * [[include:../manual/tutorial.md]]
+ */
 import React from "react";
-export const App = () => (React.createElement("section", null,
-    React.createElement("div", { className: "App" },
-        React.createElement("h1", null, "Hello Demo Component"),
+/**
+ * Simple Function Component Test
+ * @param props.bar - test prop
+ */
+export const FuncComponent = (props) => (React.createElement("section", { title: props.bar },
+    React.createElement("div", { className: "FuncComponent" },
+        React.createElement("h1", null, "Hello Demo Function Component"),
         React.createElement("h2", null, "Just showing you how"))));
+export class ClassComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (React.createElement("section", null,
+            React.createElement("div", { className: "FuncComponent", title: this.props.bar },
+                React.createElement("h1", null, "Hello Demo Function Component"),
+                React.createElement("h2", null, "Just showing you how"))));
+    }
+}
