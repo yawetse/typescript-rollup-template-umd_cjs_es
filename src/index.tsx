19d95af80b2551a41815846ee765c68d3f440eb0
yaw joseph etse <yaw.etse@gmail.com>
2020-02-25 19:32:24 -0500

    setting up basic file structure

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const App = () => (<section>
+  <div className="App">
+    <h1>Hello Demo Component</h1>
+    <h2>Just showing you how</h2>
+  </div>
+</section>);
+
